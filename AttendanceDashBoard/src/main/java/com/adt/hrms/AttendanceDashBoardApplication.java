e90d314a1cc6f1353f0f963ea76ef1110bf8af3b
MukeshC <mukeshchandalwar.adt@gmail.com>
2023-01-19 12:15:41 +0530

    test commit

--- AttendanceDashBoard/src/main/java/com/adt/hrms/AttendanceDashBoardApplication.java
+++ AttendanceDashBoard/src/main/java/com/adt/hrms/AttendanceDashBoardApplication.java
@@ -3,6 +3,11 @@ package com.adt.hrms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot Main app
+ * @author hp
+ *
+ */
 @SpringBootApplication
 public class AttendanceDashBoardApplication {
 
