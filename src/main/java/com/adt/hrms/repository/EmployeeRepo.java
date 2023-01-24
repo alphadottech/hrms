da99a4c7f782412e0cabea4a7dca85e59147e0f8
MukeshC <mukeshchandalwar.adt@gmail.com>
2023-01-24 10:50:47 +0530

    employee search by first name and last name changed in repository EmployeeRepo

--- src/main/java/com/adt/hrms/repository/EmployeeRepo.java
+++ src/main/java/com/adt/hrms/repository/EmployeeRepo.java
@@ -1,11 +1,5 @@
 package com.adt.hrms.repository;
 
-
-
-
-
-
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,21 +9,14 @@ import org.springframework.data.repository.query.Param;
 import com.adt.hrms.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-	
-//	@Query(value = "SELECT * FROM employee_details e WHERE " + "e.first_name LIKE CONCAT('%', :query, '%')" + "e.last_name LIKE CONCAT('%',:query,'%')", nativeQuery = true)
-	
-//	@Query("SELECT e FROM Employee e WHERE " + "e.fName LIKE CONCAT('%',:query,'%')" + "e.lName LIKE CONCAT('%',:query,'%')")
-//	List<Employee> SearchEmployee(String query);
-//	
 
-	
-	
-	@Query(value = "SELECT * FROM employee_details e WHERE e.first_name LIKE %:query% OR e.last_name LIKE %:query%", nativeQuery = true)
+	@Query(value = "FROM Employee e WHERE e.firstName LIKE %:query% OR e.lastName LIKE %:query%")
 	List<Employee> SearchEmployee(@Param("query") String query);
-	
+
 	@Query(value = "SELECT * FROM employee_details e WHERE e.email_id LIKE %:query% ", nativeQuery = true)
 	List<Employee> SearchByEmailId(@Param("query") String query);
-	
+
 	Employee findByEmpId(int empId);
 
+	List<Employee> findByFirstNameContaining(@Param("name") String name);
 }
