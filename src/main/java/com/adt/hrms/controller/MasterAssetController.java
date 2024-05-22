600aaaebd9f649cc4bf94b98b639255978f52ab0
Mukesh B. Chandalwar <mukeshchandalwar.adt@gmail.com>
2024-05-22 17:45:50 +0530

    crossorigin changes

--- src/main/java/com/adt/hrms/controller/MasterAssetController.java
+++ src/main/java/com/adt/hrms/controller/MasterAssetController.java
@@ -8,19 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.adt.hrms.model.MasterAsset;
 import com.adt.hrms.service.MasterAssetService;
-
-//JyotiPancholi - Jira no ->  HRMS-63(START)
 
 @RestController
 @RequestMapping("/masterAsset")
@@ -72,15 +63,10 @@ public class MasterAssetController {
 		LOGGER.info("MasterAssetService:masterAsset:SearchByAssetType info level log message");
 		return ResponseEntity.ok(service.SearchByAssetType(query));
 	}
-	//JyotiPancholi - Jira no ->  HRMS-83(END)
-	
-	//RitikaBhawsar - Jira no ->  HRMS-63(START)
 	@PreAuthorize("@auth.allow('UPDATE_MASTER_ASSET_BY_ASSET_ID')")
 	@PutMapping("/updateMasterAssetbyid")
 	public ResponseEntity<String> updateMasterAssetbyid( @RequestBody MasterAsset asset) 
 	{
-		// service.updateMasterAsset(asset);
-		//LOGGER.info("MasterAssetService:masterAsset:updateMasterAssetbyid info level log message");
 		LOGGER.info("MasterAssetService:masterAsset:updateMasterAssetById info level log message");
 		return new ResponseEntity<String>(service.updateMasterAssetById(asset), HttpStatus.OK);
 	}
@@ -93,8 +79,6 @@ public class MasterAssetController {
 		LOGGER.info("MasterAssetService:masterAsset:findAllMasterAsset info level log message");
 		return new ResponseEntity<List<MasterAsset>>(service.findAllMasterAsset(), HttpStatus.OK);
 	}
-	//RitikaBhawsar - Jira no ->  HRMS-63(END)
 
 
 }
-//JyotiPancholi - Jira no ->  HRMS-63(END)
--- src/main/java/com/adt/hrms/controller/ProjectEngagementController.java
+++ src/main/java/com/adt/hrms/controller/ProjectEngagementController.java
@@ -8,21 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.adt.hrms.model.ProjectEngagement;
 import com.adt.hrms.service.ProjectEngagementService;
 
 import jakarta.servlet.http.HttpServletRequest;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/engagement")
 public class ProjectEngagementController {
