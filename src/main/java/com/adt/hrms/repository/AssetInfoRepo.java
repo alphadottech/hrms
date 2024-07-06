921cc3052224fc28b0ab66dc87f64b4cbab83ba7
Ritika Bhawsar <ritikabhawsar.adt@gmail.com>
2024-07-06 15:00:30 +0530

    create getAllAssetInfoByAssetTypeIdAndPagination-API

--- src/main/java/com/adt/hrms/repository/AssetInfoRepo.java
+++ src/main/java/com/adt/hrms/repository/AssetInfoRepo.java
@@ -3,6 +3,8 @@ package com.adt.hrms.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -20,5 +22,8 @@ public interface AssetInfoRepo extends JpaRepository<AssetInfo, Integer> {
 
 	@Query(value = "DELETE FROM employee_schema.asset WHERE asset_type_id=?1", nativeQuery = true)
 	void deleteByAssetTypeId(Integer assetTypeId);
+
+	@Query(value = "SELECT * FROM employee_schema.asset WHERE asset_type_id = ?1", nativeQuery = true)
+	Page<AssetInfo> findListByAssetTypeIdWithPagination(Integer assetTypeId, Pageable pageable);
 
 }
